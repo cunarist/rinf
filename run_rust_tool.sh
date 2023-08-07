04b95ea8ff0cf97c6fde868ce415a40b02d04a4b
Matej Knopp <matej.knopp@gmail.com>
2023-08-07 16:04:18 +0200

    fix! only accept cargo from ~/.cargo

--- run_rust_tool.sh
+++ run_rust_tool.sh
@@ -4,16 +4,14 @@ set -e
 
 BASEDIR=$(dirname "$0")
 
-# In case we're running in environment without PATH put the default location in there
-# Put it at the front so that we don't accidentally pick up homebrew's cargo.
-PATH="~/.cargo/bin:$PATH"
+# Only use rustup cargo installation. This is to avoid picking up homebrew's cargo,
+# which does not support rustup installed targets.
+CARGO="$HOME/.cargo/bin/cargo"
 
 if ! command -v cargo --version >/dev/null 2>&1
 then
     echo "|"
-    echo "| cargo not found."
-    echo "|"
-    echo "| We looked in $PATH"
+    echo "| cargo not found. Expected in $CARGO."
     echo "|"
     echo "| Maybe you need to install Rust? It only takes a minute:"
     echo "|"
@@ -22,4 +20,4 @@ then
     exit 1
 fi
 
-cargo run --manifest-path=$BASEDIR/build_tool/Cargo.toml --bin build_tool --target-dir=$CARGOKIT_TOOL_TEMP_DIR --quiet -- $@
+$CARGO run --manifest-path=$BASEDIR/build_tool/Cargo.toml --bin build_tool --target-dir=$CARGOKIT_TOOL_TEMP_DIR --quiet -- $@
