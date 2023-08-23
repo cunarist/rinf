80743ab003048af15efd03c7cd3ee78041858a9f
book诗意 <tech.gosling0d@icloud.com>
2023-08-23 20:39:07 +0800

    Update cargokit to latest version

--- connectors/cargokit/run_rust_tool.sh
+++ connectors/cargokit/run_rust_tool.sh
@@ -4,16 +4,12 @@ set -e
 
 BASEDIR=$(dirname "$0")
 
-# In case we're running in environment without PATH put the default location in there
-# Put it at the front so that we don't accidentally pick up homebrew's cargo.
-PATH="~/.cargo/bin:$PATH"
+export PATH="~/.cargo/bin:$PATH"
 
-if ! command -v cargo --version >/dev/null 2>&1
+if ! command -v rustup --version >/dev/null 2>&1
 then
     echo "|"
-    echo "| cargo not found."
-    echo "|"
-    echo "| We looked in $PATH"
+    echo "| rustup not found. Expected in ~/.cargo/bin or \$PATH."
     echo "|"
     echo "| Maybe you need to install Rust? It only takes a minute:"
     echo "|"
@@ -22,4 +18,4 @@ then
     exit 1
 fi
 
-cargo run --manifest-path=$BASEDIR/build_tool/Cargo.toml --bin build_tool --target-dir=$CARGOKIT_TOOL_TEMP_DIR --quiet -- $@
+rustup run stable cargo run --manifest-path="$BASEDIR/build_tool/Cargo.toml" --bin build_tool --target-dir="$CARGOKIT_TOOL_TEMP_DIR" --quiet -- $@
