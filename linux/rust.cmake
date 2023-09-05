159038ba263dcbc8cde9efeaa3426ed85bc81a80
Kim Dong-Hyun <temeddix@gmail.com>
2023-09-05 22:49:27 +0900

    Fix running problems

--- linux/rust.cmake
+++ linux/rust.cmake
@@ -5,17 +5,16 @@ apply_standard_settings(${BINARY_NAME})
 set_target_properties(${BINARY_NAME} PROPERTIES
   CXX_VISIBILITY_PRESET hidden
   BUILD_RPATH_USE_ORIGIN ON
-)
+  )
 
 target_compile_definitions(${BINARY_NAME} PRIVATE FLUTTER_PLUGIN_IMPL)
-target_link_libraries(${BINARY_NAME} PRIVATE flutter PkgConfig::GTK)
+target_link_libraries(${BINARY_NAME} PRIVATE flutter)
 
 include("../cargokit/cmake/cargokit.cmake")
 apply_cargokit(${BINARY_NAME} ${CMAKE_SOURCE_DIR}/../native/hub hub "")
 target_link_libraries(${BINARY_NAME} PUBLIC hub)
 
-set(
-  PLUGIN_BUNDLED_LIBRARIES
-  ${PLUGIN_BUNDLED_LIBRARIES} $<TARGET_FILE:rust_in_flutter>
+set(rust_in_flutter_bundled_libraries
+  "${${BINARY_NAME}_cargokit_lib}"
   PARENT_SCOPE
 )
