a4db436fe34f59fce4d290bb2a17c7b548b7bf6e
Kim Dong-Hyun <temeddix@gmail.com>
2023-09-05 22:54:55 +0900

    Organize code

--- linux/rust.cmake
+++ linux/rust.cmake
@@ -2,10 +2,11 @@ cmake_policy(SET CMP0079 NEW)
 
 apply_standard_settings(${BINARY_NAME})
 
-set_target_properties(${BINARY_NAME} PROPERTIES
+set_target_properties(
+  ${BINARY_NAME} PROPERTIES
   CXX_VISIBILITY_PRESET hidden
   BUILD_RPATH_USE_ORIGIN ON
-  )
+)
 
 target_compile_definitions(${BINARY_NAME} PRIVATE FLUTTER_PLUGIN_IMPL)
 target_link_libraries(${BINARY_NAME} PRIVATE flutter)
@@ -14,7 +15,8 @@ include("../cargokit/cmake/cargokit.cmake")
 apply_cargokit(${BINARY_NAME} ${CMAKE_SOURCE_DIR}/../native/hub hub "")
 target_link_libraries(${BINARY_NAME} PUBLIC hub)
 
-set(rust_in_flutter_bundled_libraries
+set(
+  rust_in_flutter_bundled_libraries
   "${${BINARY_NAME}_cargokit_lib}"
   PARENT_SCOPE
 )
