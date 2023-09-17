db9cdccf182273f51da63dd05422382ed09c375e
Kim Dong-Hyun <temeddix@gmail.com>
2023-09-17 16:04:52 +0900

    Improve shorthand crate

--- shorthand/src/main.rs
+++ shorthand/src/main.rs
@@ -6,8 +6,12 @@ fn main() {
     let dart_command_args: Vec<String> = env::args().skip(1).collect();
 
     // Build the command to run the Dart script
-    let dart_path = which::which("dart").unwrap(); // https://github.com/rust-lang/rust/issues/37519
-    let mut command = Command::new(dart_path);
+    #[cfg(target_family = "windows")]
+    let mut command = Command::new("dart.bat");
+    #[cfg(target_family = "unix")]
+    let mut command = Command::new("dart");
+    #[cfg(target_family = "wasm")]
+    let mut command = Command::new("dart");
     command.args(["run", "rust_in_flutter"]);
     command.args(&dart_command_args);
 
