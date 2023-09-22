36c49dd388c8001ee55bd0e9643527701c76f323
Kim Dong-Hyun <temeddix@gmail.com>
2023-09-22 17:33:47 +0900

    Improve comments about web aliases

--- example/native/hub/src/web_alias.rs
+++ example/native/hub/src/web_alias.rs
@@ -1,15 +1,23 @@
-//! The web has many restrictions due to its sandboxed environment,
-//! which prevents the use of native threads, atomics, and time functionalities.
-//! Consequently, certain features are missing from Rust's `std`
-//! and other crates due to these limitations.
+//! The web has many restrictions due to its sandboxed environment
+//! which prevents the use of
+//! threads, atomics, time, file IO, network IO,
+//! and many other native functionalities.
+//! Consequently, certain features are missing from various crates
+//! including Rust's `std` due to these limitations.
 //!
 //! To address this issue, this module offers various imports
 //! with the **same names** as the original native ones,
-//! providing workarounds for these constraints
+//! providing workarounds for these constraints.
 //!
 //! You might encounter situations
 //! where you cannot use native Rust code directly on the web.
-//! Add more custom aliases if needed.
+//! Add more custom web aliases here if needed.
+//! There are many crates at `crates.io`
+//! that mimic native functionalities on the web
+//! by interacting with JavaScript,
+//! so use them if necessary.
+//!
+//! If your app is not targeting web, you can simply remove this module.
 
 #![allow(dead_code, unused_imports, unused_macros)]
 
