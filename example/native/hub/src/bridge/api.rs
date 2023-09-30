33631ec6317b3fad80cf985c1f0c1e6afa69b6dd
Kim Dong-Hyun <temeddix@gmail.com>
2023-09-30 09:21:27 +0900

    Filter backtrace frames from infrastructure crates

--- example/native/hub/src/bridge/api.rs
+++ example/native/hub/src/bridge/api.rs
@@ -165,8 +165,34 @@ pub fn start_rust_logic() {
     {
         #[cfg(debug_assertions)]
         std::panic::set_hook(Box::new(|panic_info| {
-            let backtrace = backtrace::Backtrace::new();
-            crate::debug_print!("A panic occurred in Rust.\n{}\n{:?}", panic_info, backtrace);
+            let mut frames_filtered = Vec::new();
+            backtrace::trace(|frame| {
+                // Filter some backtrace frames
+                // as those from infrastructure crates are not needed.
+                let mut should_keep_tracing = true;
+                backtrace::resolve_frame(frame, |symbol| {
+                    if let Some(symbol_name) = symbol.name() {
+                        let name = symbol_name.to_string();
+                        if name.starts_with("backtrace::") {
+                            return;
+                        }
+                        if name.starts_with("tokio::") {
+                            should_keep_tracing = false;
+                            return;
+                        }
+                    }
+                    let backtrace_frame = backtrace::BacktraceFrame::from(frame.to_owned());
+                    frames_filtered.push(backtrace_frame);
+                });
+                should_keep_tracing
+            });
+            let mut backtrace_filtered = backtrace::Backtrace::from(frames_filtered);
+            backtrace_filtered.resolve();
+            crate::debug_print!(
+                "A panic occurred in Rust.\n{}\n{:?}",
+                panic_info,
+                backtrace_filtered
+            );
         }));
         TOKIO_RUNTIME.with(move |inner| {
             let tokio_runtime = tokio::runtime::Builder::new_multi_thread()
