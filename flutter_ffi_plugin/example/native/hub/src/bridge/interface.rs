15b81acf0e2060fbda4e5584198f393a641a922a
DongHyun Kim <temeddix@gmail.com>
2024-01-17 14:58:16 +0900

    Add comments to exposed APIs

--- flutter_ffi_plugin/example/native/hub/src/bridge/interface.rs
+++ flutter_ffi_plugin/example/native/hub/src/bridge/interface.rs
@@ -11,6 +11,10 @@ pub use super::interface_os::*;
 #[cfg(target_family = "wasm")]
 pub use super::interface_web::*;
 
+// This contains a message from Dart.
+// Optionally, a custom binary called `blob` can also be included.
+// This type is generic, and the message
+// can be of any type declared in Protobuf.
 pub struct DartSignal<T> {
     pub message: T,
     pub blob: Option<Vec<u8>>,
