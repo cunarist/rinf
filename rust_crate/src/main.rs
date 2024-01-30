d21eb85d672dc15ff418b3e9e1348c0b11b9b0f0
TENX-S <ttenx@pm.me>
2024-01-30 14:15:34 +0800

    custom protoc path

--- rust_crate/src/main.rs
+++ rust_crate/src/main.rs
@@ -1,17 +1,37 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    dart_args: Vec<String>,
+    #[arg(short, long)]
+    protoc_path: Option<String>,
+}
+
 #[cfg(not(target_family = "wasm"))]
 fn main() {
     use std::env;
     use std::fs;
     use std::path;
+    use std::path::PathBuf;
     use std::process;
+    use std::str::FromStr;
 
-    // Install Protobuf compiler and get the path.
-    let home_path = home::home_dir().unwrap();
-    let out_path = home_path.join(".local").join("bin");
-    fs::create_dir_all(&out_path).unwrap();
-    env::set_var("OUT_DIR", out_path.to_str().unwrap());
-    let (protoc_binary_path, _) = protoc_prebuilt::init("25.2").unwrap();
-    let protoc_path = protoc_binary_path.parent().unwrap().to_path_buf();
+    let args = Args::parse();
+
+    let protoc_path;
+
+    if let Some(p) = args.protoc_path {
+        let provided = PathBuf::from_str(&p).unwrap();
+        protoc_path = provided;
+    } else {
+        // Install Protobuf compiler and get the path.
+        let home_path = home::home_dir().unwrap();
+        let out_path = home_path.join(".local").join("bin");
+        fs::create_dir_all(&out_path).unwrap();
+        env::set_var("OUT_DIR", out_path.to_str().unwrap());
+        let (protoc_binary_path, _) = protoc_prebuilt::init("25.2").unwrap();
+        protoc_path = protoc_binary_path.parent().unwrap().to_path_buf();
+    }
 
     // Find the path where Dart executables are located.
     #[cfg(target_family = "windows")]
@@ -33,16 +53,13 @@ fn main() {
     path_var.push(pub_cache_bin_path);
     env::set_var("PATH", env::join_paths(path_var).unwrap());
 
-    // Get command-line arguments excluding the program name.
-    let dart_command_args: Vec<String> = env::args().skip(1).collect();
-
     // Build the command to run the Dart script.
     #[cfg(target_family = "windows")]
     let mut command = process::Command::new("dart.bat");
     #[cfg(target_family = "unix")]
     let mut command = process::Command::new("dart");
     command.args(["run", "rinf"]);
-    command.args(&dart_command_args);
+    command.args(&args.dart_args);
 
     // Execute the command
     let _ = command.status();
