623268684ca83ab75f1c03d2b5756d7101b051e6
DongHyun Kim <temeddix@gmail.com>
2024-02-02 01:20:29 +0900

    Improve comments

--- flutter_ffi_plugin/example/native/hub/src/lib.rs
+++ flutter_ffi_plugin/example/native/hub/src/lib.rs
@@ -1,4 +1,9 @@
+//! This `hub` crate is the
+//! entry point of the Rust logic.
+
 // This `tokio` will be used by Rinf.
+// You can replace it with the original `tokio`
+// if you're not targeting the web.
 use tokio_with_wasm::tokio;
 
 mod messages;
@@ -6,8 +11,8 @@ mod sample_functions;
 
 rinf::write_interface!();
 
-/// This `hub` crate is the entry point for the Rust logic.
-/// Always use non-blocking async functions such as `tokio::fs::File::open`.
+/// Always use non-blocking async functions
+/// such as `tokio::fs::File::open`.
 async fn main() {
     // Repeat `tokio::spawn` anywhere in your code
     // if more concurrent tasks are needed.
