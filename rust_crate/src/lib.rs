7b92a1ac4fa34a072a66659457e61ff1b0961db9
DongHyun Kim <temeddix@gmail.com>
2024-02-02 02:01:20 +0900

    Organize code

--- rust_crate/src/lib.rs
+++ rust_crate/src/lib.rs
@@ -1,9 +1,10 @@
 pub use interface::*;
 
 pub mod externs;
+mod macros;
+
 mod interface;
 #[cfg(not(target_family = "wasm"))]
 mod interface_os;
 #[cfg(target_family = "wasm")]
 mod interface_web;
-mod macros;
