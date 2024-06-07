13273ecc642b6055939b4318de8b6c69097e8bdb
temeddix <temeddix@gmail.com>
2024-06-07 21:49:26 +0900

    New struct `RuntimeDropper`

--- rust_crate/src/macros.rs
+++ rust_crate/src/macros.rs
@@ -37,15 +37,26 @@ macro_rules! write_interface {
             use tokio::sync::mpsc::channel;
             use tokio::sync::mpsc::{Receiver, Sender};
 
+            // This is the runtime where async event loop runs.
+            type TokioRuntime = OnceLock<Mutex<Option<Runtime>>>;
+            static TOKIO_RUNTIME: TokioRuntime = OnceLock::new();
+
             // We use `os_thread_local` so that when the program fails
             // and the main thread exits unexpectedly,
             // the whole async tokio runtime can disappear as well.
-            type TokioRuntime = OnceLock<ThreadLocal<RefCell<Option<Runtime>>>>;
-            static TOKIO_RUNTIME: TokioRuntime = OnceLock::new();
+            // Without this struct, zombie threads inside the runtime
+            // might outlive the app.
+            struct RuntimeDropper;
+            impl Drop for RuntimeDropper {
+                fn drop(&mut self) {
+                    drop_tokio_runtime();
+                }
+            }
+            static RUNTIME_DROPPER: OnceLock<ThreadLocal<RuntimeDropper>> = OnceLock::new();
 
             // These channels are used for gracefully shutting down the tokio runtime.
             // Right before the topmost Flutter widget gets disposed,
-            // these channels will ensure that Rust logic is properly stopped.
+            // these channels will help ensure that Rust logic is properly stopped.
             struct ShutdownStore {
                 shutdown_sender: Option<Sender<()>>,
                 shutdown_receiver: Option<Receiver<()>>,
@@ -55,11 +66,13 @@ macro_rules! write_interface {
             static SHUTDOWN_STORE: ShutdownStoreShared = OnceLock::new();
 
             pub fn get_shutdown_receiver() -> Receiver<()> {
-                let mut guard = SHUTDOWN_STORE
-                    .get_or_init(|| Mutex::new(None))
-                    .lock()
-                    .unwrap();
+                let mut guard = SHUTDOWN_STORE.get().unwrap().lock().unwrap();
                 guard.as_mut().unwrap().shutdown_receiver.take().unwrap()
+            }
+
+            fn drop_tokio_runtime() {
+                // Dropping the tokio runtime causes it to shut down completely.
+                TOKIO_RUNTIME.get().unwrap().lock().unwrap().take();
             }
 
             #[no_mangle]
@@ -89,6 +102,10 @@ macro_rules! write_interface {
                         done_receiver,
                     });
 
+                    // Prepare to drop the async runtime
+                    // when program fails unexpectedly.
+                    RUNTIME_DROPPER.get_or_init(|| ThreadLocal::new(|| RuntimeDropper));
+
                     // Run the main function.
                     let tokio_runtime = Builder::new_multi_thread().enable_all().build().unwrap();
                     tokio_runtime.spawn(async move {
@@ -98,40 +115,28 @@ macro_rules! write_interface {
                         // tell the main thread to stop waiting.
                         let _ = done_sender.send(());
                     });
-                    let os_cell =
-                        TOKIO_RUNTIME.get_or_init(|| ThreadLocal::new(|| RefCell::new(None)));
-                    os_cell.with(move |cell| {
-                        // If there was already a tokio runtime previously,
-                        // most likely due to Dart's hot restart,
-                        // its tasks as well as itself will be terminated,
-                        // being replaced with the new one.
-                        cell.replace(Some(tokio_runtime));
-                    });
+                    let mut guard = TOKIO_RUNTIME
+                        .get_or_init(|| Mutex::new(None))
+                        .lock()
+                        .unwrap();
+                    guard.replace(tokio_runtime);
                 });
             }
 
             #[no_mangle]
             pub extern "C" fn stop_rust_logic_extern() {
-                // Tell the Rust logic to perform finalziation code.
-                let guard = SHUTDOWN_STORE
-                    .get_or_init(|| Mutex::new(None))
-                    .lock()
-                    .unwrap();
-                let _ = guard
-                    .as_ref()
-                    .unwrap()
-                    .shutdown_sender
-                    .as_ref()
-                    .unwrap()
-                    .try_send(());
-                let _ = guard.as_ref().unwrap().done_receiver.recv();
-                // Dropping the tokio runtime causes it to shut down completely.
                 let _ = catch_unwind(|| {
-                    let os_cell =
-                        TOKIO_RUNTIME.get_or_init(|| ThreadLocal::new(|| RefCell::new(None)));
-                    os_cell.with(move |cell| {
-                        cell.take();
-                    });
+                    // Tell the Rust logic to perform finalziation code.
+                    let guard = SHUTDOWN_STORE.get().unwrap().lock().unwrap();
+                    let _ = guard
+                        .as_ref()
+                        .unwrap()
+                        .shutdown_sender
+                        .as_ref()
+                        .unwrap()
+                        .try_send(());
+                    let _ = guard.as_ref().unwrap().done_receiver.recv();
+                    drop_tokio_runtime();
                 });
             }
 
@@ -143,13 +148,14 @@ macro_rules! write_interface {
                 binary_pointer: *const u8,
                 binary_size: usize,
             ) {
-                let message_bytes = unsafe {
-                    std::slice::from_raw_parts(message_pointer as *mut u8, message_size).to_vec()
-                };
-                let binary = unsafe {
-                    std::slice::from_raw_parts(binary_pointer as *mut u8, binary_size).to_vec()
-                };
                 let _ = catch_unwind(|| {
+                    let message_bytes = unsafe {
+                        std::slice::from_raw_parts(message_pointer as *mut u8, message_size)
+                            .to_vec()
+                    };
+                    let binary = unsafe {
+                        std::slice::from_raw_parts(binary_pointer as *mut u8, binary_size).to_vec()
+                    };
                     crate::messages::generated::handle_dart_signal(
                         message_id as i32,
                         message_bytes,
