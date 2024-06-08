f52b2924828f081cce50ee1126663d5b456a2d3a
Donghyun Kim <temeddix@gmail.com>
2024-06-08 15:52:38 +0900

    Fix a small bug

--- flutter_ffi_plugin/template/native/hub/src/lib.rs
+++ flutter_ffi_plugin/template/native/hub/src/lib.rs
@@ -18,7 +18,7 @@ async fn main() {
     // Send signals to Dart like below.
     SmallNumber { number: 7 }.send_signal_to_dart();
     // Get receivers that listen to Dart signals like below.
-    let receiver = SmallText::get_dart_signal_receiver();
+    let mut receiver = SmallText::get_dart_signal_receiver();
     while let Some(dart_signal) = receiver.recv().await {
         let message: SmallText = dart_signal.message;
         rinf::debug_print!("{message:?}");
