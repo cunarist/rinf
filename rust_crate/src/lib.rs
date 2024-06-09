9e123737d63dd0fc7aec8481c2f6e406c9bbed01
Donghyun Kim <temeddix@gmail.com>
2024-06-09 17:48:35 +0900

    Remove all panics from Rinf executable

--- rust_crate/src/lib.rs
+++ rust_crate/src/lib.rs
@@ -1,3 +1,4 @@
+mod common;
 mod macros;
 
 mod interface;
--- rust_crate/src/main.rs
+++ rust_crate/src/main.rs
@@ -1,39 +1,55 @@
+mod common;
+use common::*;
+
 #[cfg(not(target_family = "wasm"))]
-fn main() {
+fn main() -> Result<()> {
     use std::env;
     use std::fs;
     use std::path;
     use std::process;
 
     // Verify Protobuf compiler.
-    let protoc_path;
-    if let Ok(installed) = which::which("protoc") {
+    let protoc_path = if let Ok(installed) = which::which("protoc") {
         // Get the path of Protobuf compiler that's already installed.
         println!("Detected `protoc`, skipping auto installation.");
-        protoc_path = installed.parent().unwrap().to_path_buf();
+        installed
+            .parent()
+            .ok_or("Could not get the parent of `protoc` path.")?
+            .to_path_buf()
     } else {
         // Install Protobuf compiler and get the path.
-        let home_path = home::home_dir().unwrap();
+        let home_path =
+            home::home_dir().ok_or("Could not get home directory for `protoc` installation.")?;
         let out_path = home_path.join(".local").join("bin");
-        fs::create_dir_all(&out_path).unwrap();
-        env::set_var("OUT_DIR", out_path.to_str().unwrap());
+        fs::create_dir_all(&out_path)?;
+        env::set_var(
+            "OUT_DIR",
+            out_path
+                .to_str()
+                .ok_or("Could not set environment variable path")?,
+        );
         let install_result = protoc_prebuilt::init("25.2");
-        if install_result.is_err() {
-            println!("Automatic installation of `protoc` failed.");
-            println!("Try installing `protoc` manually and adding it to PATH.");
-        }
-        let (protoc_binary_path, _) = install_result.unwrap();
-        protoc_path = protoc_binary_path.parent().unwrap().to_path_buf();
-    }
+        let (protoc_binary_path, _) = install_result.map_err(|_| {
+            format!(
+                "{}\n{}",
+                "Automatic installation of `protoc` failed.",
+                "Try installing `protoc` manually and adding it to PATH."
+            )
+        })?;
+        protoc_binary_path
+            .parent()
+            .ok_or("Could not get the parent of installed `protoc` path.")?
+            .to_path_buf()
+    };
 
     // Find the path where Dart executables are located.
     #[cfg(target_family = "windows")]
-    let pub_cache_bin_path = path::PathBuf::from(env::var("LOCALAPPDATA").unwrap())
+    let pub_cache_bin_path = path::PathBuf::from(env::var("LOCALAPPDATA")?)
         .join("Pub")
         .join("Cache")
         .join("bin");
     #[cfg(target_family = "unix")]
-    let pub_cache_bin_path = path::PathBuf::from(env::var("HOME").unwrap())
+    let pub_cache_bin_path = path::PathBuf::from(env::var("HOME")?)
         .join(".pub-cache")
         .join("bin");
 
@@ -44,19 +60,19 @@ fn main() {
     };
     path_var.push(protoc_path);
     path_var.push(pub_cache_bin_path);
-    env::set_var("PATH", env::join_paths(path_var).unwrap());
+    env::set_var("PATH", env::join_paths(path_var)?);
 
     // Get command-line arguments excluding the program name.
     let dart_command_args: Vec<String> = env::args().skip(1).collect();
 
-    // Build the command to run the Dart script.
-    let dart_path = which::which("dart").expect("Couldn't find Dart executable");
+    // Run the Dart script.
+    let dart_path = which::which("dart")?;
     let mut command = process::Command::new(dart_path);
     command.args(["run", "rinf"]);
     command.args(&dart_command_args);
+    command.status()?;
 
-    // Execute the command
-    let _ = command.status();
+    Ok(())
 }
 
 #[cfg(target_family = "wasm")]
