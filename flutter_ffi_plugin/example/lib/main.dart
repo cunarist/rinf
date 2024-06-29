fa70b8aa3df4566b40f927be5388067ce6d9d87f
Donghyun Kim <temeddix@gmail.com>
2024-06-29 10:47:08 +0900

    Improve example app's readability

--- flutter_ffi_plugin/example/lib/main.dart
+++ flutter_ffi_plugin/example/lib/main.dart
@@ -61,71 +61,11 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // `StreamBuilder` listens to a stream
-            // and rebuilds the widget accordingly.
-            StreamBuilder(
-                stream: SampleFractal.rustSignalStream,
-                builder: (context, snapshot) {
-                  final rustSignal = snapshot.data;
-                  if (rustSignal == null) {
-                    return Container(
-                      margin: const EdgeInsets.all(20),
-                      width: 256,
-                      height: 256,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(24.0),
-                        color: Colors.black,
-                      ),
-                    );
-                  }
-                  final imageData = rustSignal.binary;
-                  return Container(
-                    margin: const EdgeInsets.all(20),
-                    width: 256,
-                    height: 256,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(24.0),
-                      child: FittedBox(
-                        fit: BoxFit.contain,
-                        child: Image.memory(
-                          imageData,
-                          width: 256,
-                          height: 256,
-                          gaplessPlayback: true,
-                        ),
-                      ),
-                    ),
-                  );
-                }),
-            StreamBuilder(
-              // This stream is generated from a marked Protobuf message.
-              stream: SampleNumberOutput.rustSignalStream,
-              builder: (context, snapshot) {
-                final rustSignal = snapshot.data;
-                // If the app has just started and widget is built
-                // without receiving a Rust signal,
-                // the snapshot data will be null.
-                // It's when the widget is being built for the first time.
-                if (rustSignal == null) {
-                  // Return the initial widget if the snapshot data is null.
-                  return Text('Initial value 0');
-                }
-                final sampleNumberOutput = rustSignal.message;
-                final currentNumber = sampleNumberOutput.currentNumber;
-                return Text('Current value is $currentNumber');
-              },
-            ),
-          ],
-        ),
-      ),
-      // This is a button that calls the generated function.
+      body: Center(child: MyColumn()),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          // The method is generated from a marked Protobuf message.
+          // The `sendSignalToRust` method is generated
+          // from a marked Protobuf message.
           SampleNumberInput(
             letter: "HELLO FROM DART!",
             dummyOne: 25,
@@ -139,6 +79,71 @@ class MyHomePage extends StatelessWidget {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
+    );
+  }
+}
+
+class MyColumn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        // `StreamBuilder` listens to a stream
+        // and rebuilds the widget accordingly.
+        StreamBuilder(
+            stream: SampleFractal.rustSignalStream,
+            builder: (context, snapshot) {
+              final rustSignal = snapshot.data;
+              if (rustSignal == null) {
+                return Container(
+                  margin: const EdgeInsets.all(20),
+                  width: 256,
+                  height: 256,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(24.0),
+                    color: Colors.black,
+                  ),
+                );
+              }
+              final imageData = rustSignal.binary;
+              return Container(
+                margin: const EdgeInsets.all(20),
+                width: 256,
+                height: 256,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(24.0),
+                  child: FittedBox(
+                    fit: BoxFit.contain,
+                    child: Image.memory(
+                      imageData,
+                      width: 256,
+                      height: 256,
+                      gaplessPlayback: true,
+                    ),
+                  ),
+                ),
+              );
+            }),
+        StreamBuilder(
+          // This stream is generated from a marked Protobuf message.
+          stream: SampleNumberOutput.rustSignalStream,
+          builder: (context, snapshot) {
+            final rustSignal = snapshot.data;
+            // If the app has just started and widget is built
+            // without receiving a Rust signal,
+            // the snapshot data will be null.
+            // It's when the widget is being built for the first time.
+            if (rustSignal == null) {
+              // Return the initial widget if the snapshot data is null.
+              return Text('Initial value 0');
+            }
+            final sampleNumberOutput = rustSignal.message;
+            final currentNumber = sampleNumberOutput.currentNumber;
+            return Text('Current value is $currentNumber');
+          },
+        ),
+      ],
     );
   }
 }
