9697c00ee226ba9d928b488b55fe00dd0ef63ace
Donghyun Kim <temeddix@gmail.com>
2024-09-10 02:31:45 +0900

    fix loading

--- flutter_package/lib/src/load_os.dart
+++ flutter_package/lib/src/load_os.dart
@@ -2,30 +2,29 @@ import 'dart:io' as io;
 import 'dart:ffi';
 
 String? dynamicLibPath;
-final rustLibrary = loadRustLibrary();
 
 void setDynamicLibPath(String path) {
   dynamicLibPath = path;
 }
 
-DynamicLibrary loadRustLibrary() {
+void loadRustLibrary() {
   // Use provided dynamic library path if possible.
   final path = dynamicLibPath;
   if (path != null) {
-    return DynamicLibrary.open(path);
+    DynamicLibrary.open(path);
   }
 
   // Otherewise, use the default path.
   if (io.Platform.isLinux) {
-    return DynamicLibrary.open('libhub.so');
+    DynamicLibrary.open('libhub.so');
   } else if (io.Platform.isAndroid) {
-    return DynamicLibrary.open('libhub.so');
+    DynamicLibrary.open('libhub.so');
   } else if (io.Platform.isWindows) {
-    return DynamicLibrary.open('hub.dll');
+    DynamicLibrary.open('hub.dll');
   } else if (io.Platform.isIOS) {
-    return DynamicLibrary.open('rinf.framework/rinf');
+    DynamicLibrary.open('rinf.framework/rinf');
   } else if (io.Platform.isMacOS) {
-    return DynamicLibrary.open('rinf.framework/rinf');
+    DynamicLibrary.open('rinf.framework/rinf');
   } else {
     throw UnsupportedError('This operating system is not supported.');
   }
