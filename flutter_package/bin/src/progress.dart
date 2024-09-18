81167837b081d830c23d0c4255a9d52c70a87337
Donghyun Kim <temeddix@gmail.com>
2024-09-18 12:01:48 +0900

    Organize progress bar code

--- flutter_package/bin/src/progress.dart
+++ flutter_package/bin/src/progress.dart
@@ -1,8 +1,7 @@
-// CLI progress bar copied from
+// CLI progress bar copied and modified from
 // https://github.com/RohitEdathil/ConsoleBars
 
 import 'dart:async';
-import 'dart:io';
 import 'common.dart';
 
 class ProgressBar {
@@ -13,7 +12,7 @@ class ProgressBar {
   int _progress = 0;
   late int max;
 
-  // Time
+  /// Tracks time
   final _clock = Stopwatch();
 
   /// Whether a timer should be present
@@ -21,8 +20,6 @@ class ProgressBar {
 
   /// Percentage should be displayed or not
   bool percentage;
-
-  // Decorations
 
   /// The description of the bar
   String _desc;
@@ -33,11 +30,8 @@ class ProgressBar {
   /// The character to used as fill
   String fill;
 
-  /// Scale of the bar relative to the terminal width
-  double scale;
-
   /// Width of the bar
-  int? width;
+  int width;
 
   /// Whether the instance should print nothing
   bool silent;
@@ -56,14 +50,13 @@ class ProgressBar {
   }
 
   /// Arguments:
-  /// - total : Total number of steps
-  /// - desc : Simple text shown before the bar (optional)
-  /// - space : Character denoting empty space (default : '.')
-  /// - fill : Character denoting filled space (default : '█')
-  /// - time : Toggle timing mode (default : false)
-  /// - percentage : Toggle percentage display (default : false)
-  /// - scale : Scale of the bar relative to width (between: 0 and 1, default: 0.5, Irrelavant if width is specified)
-  /// - width : Width of the bar drawn in the CLI
+  /// - `total` : Total number of steps
+  /// - `desc` : Simple text shown after the bar (optional)
+  /// - `space` : Character denoting empty space (default : '.')
+  /// - `fill` : Character denoting filled space (default : '█')
+  /// - `time` : Toggle timing mode (default : false)
+  /// - `percentage` : Toggle percentage display (default : false)
+  /// - `width` : Width of the bar drawn in the CLI (default: 40)
   ProgressBar({
     required int total,
     String desc = '',
@@ -71,18 +64,11 @@ class ProgressBar {
     this.fill = '█',
     this.time = false,
     this.percentage = false,
-    this.scale = 0.5,
     this.width = 40,
     this.silent = false,
   })  : _desc = desc,
         _total = total {
-    // Handles width of the bar, throws an error if it's not specified and the terminal width is not available
-    try {
-      max = width ?? ((stdout.terminalColumns - _desc.length) * scale).toInt();
-    } on StdoutException {
-      throw StdoutException(
-          'Could not get terminal width, try specifying a width manually');
-    }
+    max = width;
     if (time) {
       _clock.start();
       scheduleMicrotask(autoRender);
@@ -128,24 +114,21 @@ class ProgressBar {
         _clock.stop();
       }
     }
-    String timeStr = '';
+    String? timeStr = null;
     if (time) {
       final rate = _clock.elapsedMicroseconds / (_current == 0 ? 1 : _current);
       final eta = Duration(microseconds: ((_total - _current) * rate).toInt());
-      timeStr = '[ ' +
-          _clock.elapsed.toString().substring(0, 10) +
-          ' / ' +
-          eta.toString().substring(0, 10) +
-          ' ]';
+      final elpsedStr = _clock.elapsed.toString().substring(0, 10);
+      final etaStr = eta.toString().substring(0, 10);
+      timeStr = '[ $elpsedStr / $etaStr ]';
     }
-    String perc = '';
+    String? perc = null;
     if (percentage) {
       perc = '${(_current * 100 / _total).toStringAsFixed(1)}%';
     }
     final bar = '${fill * _progress}${space * (max - _progress)}';
     final frameParts = [bar, '$_current/$_total', perc, timeStr, ':', _desc];
-    final filteredParts = frameParts.where((v) => v.isNotEmpty).toList();
-    final frame = filteredParts.join(' ');
+    final frame = frameParts.where((v) => v != null).toList().join(' ');
     removeCliLines(1);
     print(frame);
   }
