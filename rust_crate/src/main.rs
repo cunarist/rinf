7835eabead1a9752501f619aa1a92481c54744dc
Donghyun Kim <temeddix@gmail.com>
2024-09-18 13:44:21 +0900

    Propagate exit code from Dart command

--- rust_crate/src/main.rs
+++ rust_crate/src/main.rs
@@ -8,36 +8,32 @@ fn main() -> Result<(), String> {
     // Ensure Protobuf compiler.
     let protoc_path = if let Ok(installed) = which::which("protoc") {
         // Get the path of Protobuf compiler that's already installed.
-        println!("Detected `protoc`, skipping auto installation.");
+        println!("Detected `protoc`, skipping auto installation");
         installed
             .parent()
-            .ok_or("Could not get the parent of `protoc` path.")?
+            .ok_or("Could not get the parent of `protoc` path")?
             .to_path_buf()
     } else {
         // Install Protobuf compiler and get the path.
         let home_path = home::home_dir()
-            .ok_or("Could not get home directory for `protoc` installation.")?;
+            .ok_or("Could not get home directory for `protoc` installation")?;
         let out_path = home_path.join(".local").join("bin");
         fs::create_dir_all(&out_path).map_err(|_| {
-            "Could not create the folder for `protoc` installation."
+            "Could not create the folder for `protoc` installation"
         })?;
         env::set_var(
             "OUT_DIR",
             out_path
                 .to_str()
-                .ok_or("Could not set the path for `protoc` installation.")?,
+                .ok_or("Could not set the path for `protoc` installation")?,
         );
         let install_result = protoc_prebuilt::init("25.2");
-        let (protoc_binary_path, _) = install_result.map_err(|_| {
-            format!(
-                "{}\n{}",
-                "Automatic installation of `protoc` failed.",
-                "Try installing `protoc` manually and adding it to PATH."
-            )
-        })?;
+        let (protoc_binary_path, _) = install_result.map_err(|_| 
+            "Automatic installation of `protoc` failed, try installing it manually"
+        )?;
         protoc_binary_path
             .parent()
-            .ok_or("Could not get the parent of installed `protoc` path.")?
+            .ok_or("Could not get the parent of newly installed `protoc` path")?
             .to_path_buf()
     };
 
@@ -45,14 +41,14 @@ fn main() -> Result<(), String> {
     #[cfg(target_family = "windows")]
     let pub_cache_bin_path = path::PathBuf::from(
         env::var("LOCALAPPDATA")
-            .map_err(|_| "Could not get `LOCALAPPDATA` path.")?,
+            .map_err(|_| "Could not get `LOCALAPPDATA` path")?,
     )
     .join("Pub")
     .join("Cache")
     .join("bin");
     #[cfg(target_family = "unix")]
     let pub_cache_bin_path = path::PathBuf::from(
-        env::var("HOME").map_err(|_| "Could get find `HOME` path.")?,
+        env::var("HOME").map_err(|_| "Could get find `HOME` path")?,
     )
     .join(".pub-cache")
     .join("bin");
@@ -67,7 +63,7 @@ fn main() -> Result<(), String> {
     env::set_var(
         "PATH",
         env::join_paths(path_var)
-            .map_err(|_| "Could not push required values to `PATH`.")?,
+            .map_err(|_| "Could not push required values to `PATH`")?,
     );
 
     // Get command-line arguments excluding the program name.
@@ -75,11 +71,15 @@ fn main() -> Result<(), String> {
 
     // Run the Dart script.
     let dart_path = which::which("dart")
-        .map_err(|_| "Could not find where Dart is located.")?;
+        .map_err(|_| "Could not find where Dart is located")?;
     let mut command = process::Command::new(dart_path);
     command.args(["run", "rinf"]);
     command.args(&dart_command_args);
-    command.status().map_err(|_| "Rinf command has failed.")?;
+    let exit_status = command.status().map_err(|_| "Could not run Rinf command")?;
+    let exit_code = exit_status.code().ok_or("Could not get Rinf command exit code")?;
+    if exit_code != 0 {
+        process::exit(exit_code);
+    }
 
     Ok(())
 }
