e4dc4781424cd9272ed316a8df62250b6bf0b8a5
Donghyun Kim <temeddix@gmail.com>
2024-09-27 23:38:24 +0900

    Improve comments

--- flutter_package/example/native/hub/src/common.rs
+++ flutter_package/example/native/hub/src/common.rs
@@ -3,5 +3,5 @@ use std::error::Error;
 /// This `Result` type alias allows handling any error type
 /// that implements the `Error` trait.
 /// In practice, it is recommended to use custom solutions
-/// or crates dedicated to error handling.
+/// or crates like `anyhow` dedicated to error handling.
 pub type Result<T> = std::result::Result<T, Box<dyn Error + Send + Sync>>;
