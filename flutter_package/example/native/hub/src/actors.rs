b7ee03bb42a8f41d038f279b06c96c2872e4f3f5
Donghyun Kim <temeddix@gmail.com>
2024-09-29 21:02:00 +0900

    Shorten actor names

--- flutter_package/example/native/hub/src/actors.rs
+++ flutter_package/example/native/hub/src/actors.rs
@@ -13,10 +13,10 @@ use messages::prelude::*;
 /// This struct is used to enable communication between the two actors.
 #[derive(Clone)]
 pub struct Addresses {
-    /// Address of `SampleActorOne`.
-    pub _one_addr: Address<SampleActorOne>,
-    /// Address of `SampleActorTwo`.
-    pub _two_addr: Address<SampleActorTwo>,
+    /// Address of `ActorOne`.
+    pub _one_addr: Address<ActorOne>,
+    /// Address of `ActorTwo`.
+    pub _two_addr: Address<ActorTwo>,
 }
 
 /// Creates and spawns the actors in the async system.
@@ -34,9 +34,9 @@ pub async fn create_actors() -> Result<()> {
     };
 
     // Spawn actors.
-    let actor = SampleActorOne::new(addresses.clone()).await?;
+    let actor = ActorOne::new(addresses.clone()).await?;
     spawn(one_context.run(actor));
-    let actor = SampleActorTwo::new(addresses.clone()).await?;
+    let actor = ActorTwo::new(addresses.clone()).await?;
     spawn(two_context.run(actor));
 
     Ok(())
@@ -48,8 +48,8 @@ mod letters {
     pub struct LetterTwo;
 }
 
-use sample_actor_one::SampleActorOne;
-use sample_actor_two::SampleActorTwo;
+use sample_actor_one::ActorOne;
+use sample_actor_two::ActorTwo;
 
 mod sample_actor_one {
     use crate::actors::letters::{LetterOne, LetterTwo};
@@ -59,16 +59,16 @@ mod sample_actor_one {
 
     /// An actor that manages its state and handles messages.
     /// It can send and receive messages to interact with other actors.
-    pub struct SampleActorOne {
+    pub struct ActorOne {
         /// Holds the addresses of other actors for communication.
         _addresses: Addresses,
         /// A dummy field to demonstrate actor state management.
         _dummy_field: i32,
     }
 
-    impl SampleActorOne {
+    impl ActorOne {
         pub async fn new(addresses: Addresses) -> Result<Self> {
-            let actor = SampleActorOne {
+            let actor = ActorOne {
                 _addresses: addresses,
                 _dummy_field: 3,
             };
@@ -81,19 +81,19 @@ mod sample_actor_one {
         }
     }
 
-    /// Implementing the `Actor` trait for `SampleActorOne`.
-    /// This defines `SampleActorOne` as an actor in the async system.
-    impl Actor for SampleActorOne {}
+    /// Implementing the `Actor` trait for `ActorOne`.
+    /// This defines `ActorOne` as an actor in the async system.
+    impl Actor for ActorOne {}
 
     #[async_trait]
-    impl Handler<LetterOne> for SampleActorOne {
+    impl Handler<LetterOne> for ActorOne {
         type Result = ();
         /// Handles messages received by the actor.
         async fn handle(&mut self, _msg: LetterOne, _context: &Context<Self>) {}
     }
 
     #[async_trait]
-    impl Handler<LetterTwo> for SampleActorOne {
+    impl Handler<LetterTwo> for ActorOne {
         type Result = ();
         /// Handles messages received by the actor.
         async fn handle(&mut self, _msg: LetterTwo, _context: &Context<Self>) {}
@@ -108,16 +108,16 @@ mod sample_actor_two {
 
     /// An actor that manages its state and handles messages.
     /// It can send and receive messages to interact with other actors.
-    pub struct SampleActorTwo {
+    pub struct ActorTwo {
         /// Holds the addresses of other actors for communication.
         _addresses: Addresses,
         /// A dummy field to demonstrate actor state management.
         _dummy_field: i32,
     }
 
-    impl SampleActorTwo {
+    impl ActorTwo {
         pub async fn new(addresses: Addresses) -> Result<Self> {
-            let actor = SampleActorTwo {
+            let actor = ActorTwo {
                 _addresses: addresses,
                 _dummy_field: 3,
             };
@@ -130,19 +130,19 @@ mod sample_actor_two {
         }
     }
 
-    /// Implementing the `Actor` trait for `SampleActorTwo`.
-    /// This defines `SampleActorOne` as an actor in the async system.
-    impl Actor for SampleActorTwo {}
+    /// Implementing the `Actor` trait for `ActorTwo`.
+    /// This defines `ActorOne` as an actor in the async system.
+    impl Actor for ActorTwo {}
 
     #[async_trait]
-    impl Handler<LetterOne> for SampleActorTwo {
+    impl Handler<LetterOne> for ActorTwo {
         type Result = ();
         /// Handles messages received by the actor.
         async fn handle(&mut self, _msg: LetterOne, _context: &Context<Self>) {}
     }
 
     #[async_trait]
-    impl Handler<LetterTwo> for SampleActorTwo {
+    impl Handler<LetterTwo> for ActorTwo {
         type Result = ();
         /// Handles messages received by the actor.
         async fn handle(&mut self, _msg: LetterTwo, _context: &Context<Self>) {}
