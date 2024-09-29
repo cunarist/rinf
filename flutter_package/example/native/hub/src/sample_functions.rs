349d6f52712edefe038dac9efaed3c0dcc0398f6
Donghyun Kim <temeddix@gmail.com>
2024-09-29 21:55:12 +0900

    Organize code

--- flutter_package/example/native/hub/src/sample_functions.rs
+++ flutter_package/example/native/hub/src/sample_functions.rs
@@ -67,18 +67,6 @@ pub async fn stream_fractal() {
     });
 }
 
-// A dummy function that uses sample messages to eliminate warnings.
-#[allow(dead_code)]
-async fn use_messages() {
-    let _ = SampleInput::get_dart_signal_receiver();
-    SampleOutput {
-        kind: 3,
-        oneof_input: Some(sample_output::OneofInput::Age(25)),
-    }
-    .send_signal_to_dart();
-    let _ = DeeperDummy {};
-}
-
 // Business logic for testing various crates.
 pub async fn run_debug_tests() -> Result<()> {
     if !IS_DEBUG_MODE {
@@ -190,4 +178,16 @@ pub async fn run_debug_tests() -> Result<()> {
     });
 
     Ok(())
+}
+
+// A dummy function that uses sample messages to eliminate warnings.
+#[allow(dead_code)]
+async fn use_messages() {
+    let _ = SampleInput::get_dart_signal_receiver();
+    SampleOutput {
+        kind: 3,
+        oneof_input: Some(sample_output::OneofInput::Age(25)),
+    }
+    .send_signal_to_dart();
+    let _ = DeeperDummy {};
 }
