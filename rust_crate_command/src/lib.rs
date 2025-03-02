6553fbbf317608728cf99a2e12d21b8e12642aba
Donghyun Kim <temeddix@gmail.com>
2025-03-02 15:49:28 +0900

    Remove unneeded test code

--- rust_crate_command/src/lib.rs
+++ rust_crate_command/src/lib.rs
@@ -2,21 +2,4 @@ mod parse;
 
 pub use parse::generate_dart_code;
 
-mod outer {
-    mod inner {
-        #[no_mangle]
-        pub extern "Rust" fn expose_hidden() {
-            println!("Hello from hidden function!");
-        }
-    }
-}
-
-extern "Rust" {
-    fn expose_hidden();
-}
-
-unsafe fn test_hidden() {
-    expose_hidden();
-}
-
 // TODO: Remove all panickine code like `unwrap` or `expect`.
