3b12e47769ca41ea61f0a8e9a4b18c4cc3788b76
Donghyun Kim <temeddix@gmail.com>
2025-03-09 21:54:49 +0900

    Clarify NDK version in example app

--- flutter_package/example/android/app/build.gradle.kts
+++ flutter_package/example/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.example_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
