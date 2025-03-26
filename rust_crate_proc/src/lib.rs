433878f59638d927ed0e8c572624256939bef5e6
Donghyun Kim <temeddix@gmail.com>
2025-03-26 19:09:46 +0900

    Add a todo comment

--- rust_crate_proc/src/lib.rs
+++ rust_crate_proc/src/lib.rs
@@ -44,6 +44,8 @@ fn derive_dart_signal_real(input: TokenStream) -> TokenStream {
   let extern_fn_name = &format!("rinf_send_dart_signal_{}", snake_name);
   let extern_fn_ident = Ident::new(extern_fn_name, name.span());
 
+  // TODO: Deprecate `get_dart_signal_receiver` and make `listen` method instead.
+
   let expanded = quote! {
       impl #name {
           pub fn get_dart_signal_receiver(
