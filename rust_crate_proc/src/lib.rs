91304981b2a8779e01883b96ca7fa08670883f6b
Donghyun Kim <temeddix@gmail.com>
2025-03-26 22:51:40 +0900

    Fix a typo

--- rust_crate_proc/src/lib.rs
+++ rust_crate_proc/src/lib.rs
@@ -231,7 +231,7 @@ fn derive_rust_signal_real(
 }
 
 /// Enforces all fields of a struct to have the foreign signal trait.
-/// This assists type-safe development.
+/// This assists with type-safe development.
 fn get_fields_where_clause(
   data_struct: &DataStruct,
 ) -> proc_macro2::TokenStream {
