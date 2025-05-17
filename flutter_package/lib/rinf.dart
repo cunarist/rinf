9ae1aadcbb7b277cf64758cae4d99b4751644bee
Danny Kim <temeddix@gmail.com>
2025-05-17 23:37:25 +0900

    Update the doc comment

--- flutter_package/lib/rinf.dart
+++ flutter_package/lib/rinf.dart
@@ -30,10 +30,12 @@ Future<void> initializeRust(
 }
 
 /// Terminates all Rust tasks by dropping the async runtime.
-/// Calling this function before closing the Flutter app
-/// can prevent potential resource leaks.
-/// Please note that on the web, this function does not have any effect,
-/// as tasks are managed by the JavaScript runtime, not Rust.
+/// This function blocks the Dart thread
+/// until the async runtime in Rust is completely dropped.
+/// It's recommended to call this before closing your Flutter app
+/// to prevent potential resource leaks from Rust.
+/// On the web, this function has no effect as tasks are managed by
+/// the JavaScript runtime rather than the Rust async runtime.
 void finalizeRust() {
   stopRustLogicReal();
 }
