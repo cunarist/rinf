c84aa6188ffad7e47d164455a605955e65b3cef4
Danny Kim <temeddix@gmail.com>
2025-10-03 21:31:27 +0900

    Update CI

--- rust_crate_cli/src/tool/template.rs
+++ rust_crate_cli/src/tool/template.rs
@@ -28,7 +28,11 @@ pub fn apply_rust_template(
   update_readme(root_dir)?;
 
   // Add Dart dependencies.
-  Command::new(DART_BIN)
+  Command::new(DART_BIN  )
+
+
+
+  
     .args(["pub", "add", "meta"])
     .output()?
     .capture_err()?;
